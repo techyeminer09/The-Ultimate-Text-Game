1d45136c87886794b4e610f47e75c4a3e3fa225f
techyeminer09 <109733090+techyeminer09@users.noreply.github.com>
2023-09-19 12:01:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import time as tm
 import random as ran
-money = 0
-print("Welcome To The Ultimate Text Game By techyeminer09")
-name=input("Name:")
-tm.sleep(1)
-print(name,"slowly got out of bed and wondered where they were.")
+money = 20
+def start():
+    print("Welcome To The Ultimate Text Game By techyeminer09")
+    name=input("Name:")
+    tm.sleep(1)
+    print(name,"slowly got out of bed and wondered where they were.")
+    tm.sleep(1)
+    print("You find $20 on your bedside table.")
+    tm.sleep(0.5)
+    print("You decide to start a farm.")
+def shop():
+    print("You enter the shop")
+
